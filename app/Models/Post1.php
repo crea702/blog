d5fac7201692494a0c6017a0117a57f08600a86f
Crea702 <sir.02.aib@gmail.com>
2023-09-21 16:19:37 +0500

    php artisan migrate

---
+++ app/Models/Post1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post1 extends Model
+{
+    use HasFactory;
+
+    protected $table = 'post1s';
+    protected $guarded = false;
+}
